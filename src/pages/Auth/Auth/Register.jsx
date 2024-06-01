8f0a74473ee8a40b1de8bcf172442f70ccedfb38
hammadsadi <hammad.sadi@yahoo.com>
2024-06-02 01:06:59 +0600

    Added Sign-up Page

--- src/pages/Auth/Auth/Register.jsx
+++ src/pages/Auth/Auth/Register.jsx
@@ -1,8 +1,254 @@
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import uploadPhotoToCloud from "../../../utils/uploadImageToCLoud";
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [districtList, setDistrictsList] = useState([]);
+  const [upozilaList, setUpozilaList] = useState([]);
+  // Get District
+  useEffect(() => {
+    fetch("districts.json")
+      .then((res) => res.json())
+      .then((data) => setDistrictsList(data));
+  }, []);
+  // Get Upozila
+  useEffect(() => {
+    fetch("upozila.json")
+      .then((res) => res.json())
+      .then((data) => setUpozilaList(data));
+  }, []);
 
-export default Register
+  // Handle Register Form
+  const onSubmit = async (data) => {
+    try {
+      const imgLink = await uploadPhotoToCloud(data.photo[0]);
+      const userData = {
+        name: data?.name,
+        email: data?.email,
+        photo: imgLink.data.display_url,
+        blood: data?.blood,
+        district: data?.district,
+        upazila: data?.upazila,
+        password: data?.password,
+      };
+      console.log(userData);
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    const imgLink = await uploadPhotoToCloud(data.photo[0]);
+    console.log(imgLink.data.display_url);
+  };
+
+  return (
+    <div className="flex justify-center h-full items-center my-5">
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl shadow-xl dark:bg-gray-50 dark:text-gray-800">
+        <div className="mb-8 text-center">
+          <h1 className="my-3 text-4xl font-bold">Create Account</h1>
+          <p className="text-sm text-gray-600">
+            Sign Up to access More Service
+          </p>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="name" className="text-sm">
+                  Name
+                </label>
+              </div>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                {...register("name", { required: "Name Field is required" })}
+                placeholder=" Your Name"
+                className="w-full px-3 py-2 border rounded-md bg-gray-50 focus:outline-none focus:border-primary"
+              />
+              {errors.name && (
+                <p className="text-red-600">{errors.name.message}</p>
+              )}
+            </div>
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="email" className="text-sm">
+                  Email
+                </label>
+              </div>
+              <input
+                type="email"
+                name="email"
+                {...register("email", { required: "Email Field is required" })}
+                id="email"
+                placeholder=" Your Email"
+                className="w-full px-3 py-2 border rounded-md bg-gray-50 focus:outline-none focus:border-primary"
+              />
+              {errors.email && (
+                <p className="text-red-600">{errors.email.message}</p>
+              )}
+            </div>
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="photo" className="text-sm">
+                  Photo
+                </label>
+              </div>
+              <input
+                type="file"
+                {...register("photo")}
+                id="photo"
+                name="photo"
+                className="w-full px-3 py-2 border rounded-md bg-gray-50 focus:outline-none focus:border-primary"
+              />
+            </div>
+
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="Blood" className="text-sm">
+                  Blood Group
+                </label>
+              </div>
+              <select
+                name="blood"
+                {...register("blood", {
+                  required: "Blood Field is required",
+                })}
+                id=""
+                className="w-full px-3 py-2 border rounded-md bg-gray-50 focus:outline-none focus:border-primary"
+              >
+                <option value="a+">A+</option>
+                <option value="a-">A-</option>
+                <option value="b+">B+</option>
+                <option value="b-">B-</option>
+                <option value="ab+">AB+</option>
+                <option value="ab-">AB-</option>
+                <option value="o+">O+</option>
+                <option value="o-">O-</option>
+              </select>
+              {errors.blood && (
+                <p className="text-red-600">{errors.blood.message}</p>
+              )}
+            </div>
+
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="district" className="text-sm">
+                  District
+                </label>
+              </div>
+              <select
+                name="district"
+                {...register("district", {
+                  required: "District Field is required",
+                })}
+                id="district"
+                className="w-full px-3 py-2 border rounded-md bg-gray-50 focus:outline-none focus:border-primary"
+              >
+                {districtList?.map((dis) => (
+                  <option value={dis?.name} key={dis.id}>
+                    {dis?.name}
+                  </option>
+                ))}
+              </select>
+              {errors.district && (
+                <p className="text-red-600">{errors.district.message}</p>
+              )}
+            </div>
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="upazila" className="text-sm">
+                  Upazila
+                </label>
+              </div>
+              <select
+                name="upazila"
+                {...register("upazila", {
+                  required: "Upazila Field is required",
+                })}
+                id="upazila"
+                className="w-full px-3 py-2 border rounded-md bg-gray-50 focus:outline-none focus:border-primary"
+              >
+                {upozilaList?.map((dis) => (
+                  <option value={dis?.name} key={dis.id}>
+                    {dis?.name}
+                  </option>
+                ))}
+              </select>
+              {errors.upazila && (
+                <p className="text-red-600">{errors.upazila.message}</p>
+              )}
+            </div>
+
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="password" className="text-sm">
+                  Password
+                </label>
+              </div>
+              <input
+                type="password"
+                name="password"
+                {...register("password", {
+                  required: "Password Field is required",
+                })}
+                id="password"
+                placeholder="*****"
+                className="w-full px-3 py-2 border rounded-md bg-gray-50 focus:outline-none focus:border-primary"
+              />
+              {errors.password && (
+                <p className="text-red-600">{errors.password.message}</p>
+              )}
+            </div>
+
+            <div>
+              <div className="flex justify-between mb-2">
+                <label htmlFor="confirmPassword" className="text-sm">
+                  Confirm Password
+                </label>
+              </div>
+              <input
+                type="password"
+                name="confirmPassword"
+                {...register("confirmPassword", {
+                  required: "Confirm Password Field is required",
+                })}
+                id="confirmPassword"
+                placeholder="*****"
+                className="w-full px-3 py-2 border rounded-md bg-gray-50 focus:outline-none focus:border-primary"
+              />
+              {errors.confirmPassword && (
+                <p className="text-red-600">{errors.confirmPassword.message}</p>
+              )}
+            </div>
+          </div>
+          <div className="space-y-2">
+            <div>
+              <button
+                type="submit"
+                className="w-full px-8 py-3 font-semibold rounded-md bg-primary text-white hover:bg-primary-opacity transition duration-300"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <p className="px-6 text-sm text-center dark:text-gray-600">
+          Already have an account?
+          <Link to="/login" className="hover:underline text-primary">
+            &nbsp; Login
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/utils/uploadImageToCLoud.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const uploadPhotoToCloud = async (image) => {
+  const formData = new FormData();
+  formData.append("image", image);
+  const { data } = await axios.post(
+    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMAGE_API_KEY}`,
+    formData
+  );
+  return data;
+};
+
+export default uploadPhotoToCloud;
